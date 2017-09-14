3349e1cc27d5a9491a993b7810ef348d43335c3b
Sumeet Jain <sumeet@sumeetjain.com>
2017-09-14 17:06:36 -0500

    Since I'll be adding multiple collections, give this one a name.

--- config.rb
+++ config.rb
@@ -22,7 +22,8 @@ page '/*.txt', layout: false
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
+  blog.prefix = "blog"
+  blog.name   = "blog"
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
